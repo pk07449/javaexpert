17b4f7138870de4764bd985c95ba452f6dc10d77
Pankaj kumar <java.j2ee.pankaj@gmail.com>
2017-08-12 07:14:56 +0530

    fix : fixed compilation error and corrected logic for fab series.

--- src/main/java/com/pnakaj/algo/FabonicalSeriesWithRecursion.java
+++ src/main/java/com/pnakaj/algo/FabonicalSeriesWithRecursion.java
@@ -10,11 +10,14 @@ public class FabonicalSeriesWithRecursion {
     }
 
     private static void generateFabonicalSeries(int seriesNumberUpTo, int first,int second) {
+        if(first == 0 && second == 0){
+            System.out.println(first);
+        }
         if(seriesNumberUpTo > 0) {
-            int x = second == 0 ? 1 : first + second;
+            int tempSecondNumber = second == 0 ? 1 : first + second;
 
-            System.out.println(x);
-            generateFabonicalSeries(seriesNumberUpTo-1, second, x);
+            System.out.println(tempSecondNumber);
+            generateFabonicalSeries(seriesNumberUpTo-1, second, tempSecondNumber);
         }
 
 
