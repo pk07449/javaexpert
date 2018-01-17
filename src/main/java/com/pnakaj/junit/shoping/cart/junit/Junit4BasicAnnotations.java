820413b48d10d09d2bc2f102600de7430d9f76c9
Pankaj kumar <java.j2ee.pankaj@gmail.com>
2018-01-18 02:03:20 +0530

    Added basic annotations..

---
+++ src/main/java/com/pnakaj/junit/shoping/cart/junit/Junit4BasicAnnotations.java
@@ -0,0 +1,48 @@
+package com.pnakaj.junit.shoping.cart.junit;
+
+import org.junit.*;
+
+
+/**
+ * Created by pankaj on 1/18/2018.
+ */
+public class Junit4BasicAnnotations {
+    // Run once, e.g. Database connection, connection pool
+    @BeforeClass
+    public static void runOnceBeforeClass() {
+        System.out.println("@BeforeClass - runOnceBeforeClass");
+    }
+
+    // Run once, e.g close connection, cleanup
+    @AfterClass
+    public static void runOnceAfterClass() {
+        System.out.println("@AfterClass - runOnceAfterClass");
+    }
+
+    @Before
+    public void runBeforeTestMethod() {
+        System.out.println("@Before - runBeforeTestMethod");
+    }
+
+    @After
+    public void runAfterTestMethod() {
+        System.out.println("@After - runAfterTestMethod");
+    }
+
+    @Test
+    public void test_method_1() {
+        System.out.println("@Test - test_method_1");
+    }
+
+    @Test
+    public void test_method_2() {
+        System.out.println("@Test - test_method_2");
+    }
+
+    @Test
+    @Ignore
+    public void test_method_3() {
+        System.out.println("@Test - test_method_3");
+    }
+}
+
