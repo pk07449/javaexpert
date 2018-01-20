644db380f5c9894dd02880d877ed788214ad55e9
Pankaj kumar <java.j2ee.pankaj@gmail.com>
2018-01-20 23:02:50 +0530

    Added Productin code Before applying ISP pattern

---
+++ src/main/java/com/pnakaj/junit/shoping/cart/tdd/patterns/srp/beforerefactor/isp/beforerefactor/Parshe.java
@@ -0,0 +1,29 @@
+package com.pnakaj.junit.shoping.cart.tdd.patterns.srp.beforerefactor.isp.beforerefactor;
+
+/**
+ * Created by pankaj on 1/20/2018.
+ */
+public class Parshe implements Vehicle {
+    private int gear = 1;
+    @Override
+    public void drive() {
+        System.out.println("Driving...");
+    }
+
+    @Override
+    public void changeGear(int gear) {
+       this.gear = gear;
+    }
+
+    @Override
+    public void playPod() {
+
+        System.out.println("Playing music from the ipod ....");
+
+    }
+
+    @Override
+    public int getGear() {
+        return gear;
+    }
+}
