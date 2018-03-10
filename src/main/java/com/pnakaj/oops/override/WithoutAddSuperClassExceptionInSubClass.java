75061880dc4bf66eef0606fd798af1b282855915
Pankaj kumar <java.j2ee.pankaj@gmail.com>
2018-03-10 23:38:04 +0530

    sub class avoided to add super class method signature as exception.

---
+++ src/main/java/com/pnakaj/oops/override/WithoutAddSuperClassExceptionInSubClass.java
@@ -0,0 +1,27 @@
+package com.pnakaj.oops.override;
+
+import java.io.IOException;
+
+/**
+ * Created by pankaj on 3/10/2018.
+ */
+
+public class WithoutAddSuperClassExceptionInSubClass {
+
+    public static void main(String[] args)  {
+        Hello hello = new Hai();
+        hello.m1();
+    }
+}
+
+class Hello {
+    protected void m1() throws RuntimeException {
+        System.out.println("Hello.m1");
+    }
+}
+
+class Hai extends Hello {
+    public void m1() {
+        System.out.println("Hai.m1");
+    }
+}
