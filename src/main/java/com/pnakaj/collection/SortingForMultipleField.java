b7b227ab7078c44f31feec2d7288e4c122f094db
Pankaj kumar <java.j2ee.pankaj@gmail.com>
2018-03-19 02:11:35 +0530

    Added sorring logic for multiple fields

---
+++ src/main/java/com/pnakaj/collection/SortingForMultipleField.java
@@ -0,0 +1,68 @@
+package com.pnakaj.collection;
+
+import com.pnakaj.spring.ioc.order.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+import static java.util.Comparator.comparing;
+
+/**
+ * Created by pankaj on 3/19/2018.
+ */
+public class SortingForMultipleField {
+
+    public static void main(String[] args) {
+
+        List<Customer> customers = Arrays.asList(
+        new Customer(1,"Z","50"),
+        new Customer(1,"A","5050"),
+        new Customer(1,"A","10"),
+        new Customer(11,"X","20"),
+        new Customer(1,"C","20")
+        );
+
+        customers.sort(comparing(customer -> customer.getId()));
+
+        System.out.println(customers);
+
+    }
+
+    private static class Customer{
+        int id;
+        String name;
+        String age;
+
+        public Customer(int id, String name, String age) {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getAge() {
+            return age;
+        }
+
+        @Override
+        public String
+        toString() {
+            return "Customer{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    ", age='" + age + '\'' +
+                    '}';
+        }
+    }
+}
+
+
