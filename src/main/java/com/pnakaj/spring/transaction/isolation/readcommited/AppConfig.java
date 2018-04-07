53f836e3d7df07cfb35037c2d6ad738095055d70
Pankaj kumar <java.j2ee.pankaj@gmail.com>
2018-04-08 00:49:05 +0530

    added read commited isolation leveL with hibernate.

---
+++ src/main/java/com/pnakaj/spring/transaction/isolation/readcommited/AppConfig.java
@@ -0,0 +1,59 @@
+package com.pnakaj.spring.transaction.isolation.readcommited;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.orm.hibernate3.HibernateTransactionManager;
+import org.springframework.orm.hibernate3.LocalSessionFactoryBean;
+import org.springframework.orm.hibernate3.annotation.AnnotationSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.util.Properties;
+
+/**
+ * Created by pankaj on 2/27/2017.
+ */
+@Configuration
+@ComponentScan("com.pnakaj.spring.transaction.isolation.readcommited")
+@EnableTransactionManagement(proxyTargetClass = true)
+public class AppConfig {
+
+    @Bean(destroyMethod = "close", name = "dataSource")
+    public BasicDataSource getBasicDataSource() {
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl("jdbc:oracle:thin:@localhost:1521:system");
+        basicDataSource.setUsername("system");
+        basicDataSource.setPassword("password");
+        basicDataSource.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+//        <property name="defaultAutoCommit" value="false" />
+        basicDataSource.setDefaultAutoCommit(false);
+        return basicDataSource;
+    }
+
+    @Bean
+    @Autowired
+    public HibernateTransactionManager getHibernateTransactionManager(SessionFactory sessionFactory){
+        return  new HibernateTransactionManager(sessionFactory);
+    }
+
+    @Bean
+    @DependsOn("dataSource")
+    public LocalSessionFactoryBean getLocalSessionFactory() {
+        AnnotationSessionFactoryBean localSessionFactoryBean = new AnnotationSessionFactoryBean();
+        localSessionFactoryBean.setDataSource(getBasicDataSource());
+        localSessionFactoryBean.setPackagesToScan("com.pnakaj.spring.transaction.isolation.readcommited.domain");
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.put("hibernate.dialect", "org.hibernate.dialect.OracleDialect");
+//        hibernateProperties.put("hibernate.current_session_context_class", "thread");
+        hibernateProperties.put("hibernate.show_sql", "true");
+        hibernateProperties.put("hibernate.hbm2ddl.auto", "create");
+
+        localSessionFactoryBean.setHibernateProperties(hibernateProperties);
+        return localSessionFactoryBean;
+    }
+}
+//
